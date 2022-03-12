69b444cb209af48a232df2ea66b2a0676bf2e34c
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-12 22:43:06 +0700

    using route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
