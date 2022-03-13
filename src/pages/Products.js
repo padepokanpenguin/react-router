1cdab25f710660f00f8d63e6002134ddd1bfc5ab
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-13 13:34:50 +0700

    nested route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+const {Link} = require("react-router-dom/cjs/react-router-dom.min")
+
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online Course</li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online Course</Link>
+        </li>
       </ul>
     </section>
   )
